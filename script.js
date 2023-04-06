2d252ae5ea7ff0f39fd4a2788e44b2955521ad60
Blanka Pałucka <blankapalucka@gmail.com>
2023-04-06 10:29:19 +0200

    code clean-up

--- script.js
+++ script.js
@@ -3,9 +3,8 @@
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
-            content: newTaskContent,
+            content: newTaskContent
         });
-
         render();
     }
 
@@ -20,51 +19,37 @@
     }
 
     const bindEvents = () => {
-
         const removeButtons = document.querySelectorAll(".js-remove");
-
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
-
             });
-
         });
 
         const toggleDoneButtons = document.querySelectorAll(".js-done");
-
         toggleDoneButtons.forEach((toggleDoneButton, index) => {
             toggleDoneButton.addEventListener("click", () => {
                 toggleTaskDone(index);
-
             });
-
         });
     }
 
     const render = () => {
-
         let htmlString = "";
-
         for (const task of tasks) {
             htmlString += `
-            
-            <li class="list__item">
-            <button class="js-done tasks__buton tasks__button--toggleDone">        
-                ${task.done ? "✔" : ""}
-            </button>
-            <span class="${task.done ? " list__item--done" : ""}">
-                ${task.content}
-            </span>
-            <button class="js-remove tasks__button--remove">🗑️</button>
-            </li>
+                <li class="list__item">
+                <button class="js-done tasks__buton tasks__button--toggleDone">        
+                    ${task.done ? "✔" : ""}
+                </button>
+                <span class="${task.done ? " list__item--done" : ""}">
+                    ${task.content}
+                </span>
+                <button class="js-remove tasks__button--remove">🗑️</button>
+                </li>
             `;
-
-
         };
-
         document.querySelector(".js-tasks").innerHTML = htmlString;
-
         bindEvents();
     };
 
@@ -83,13 +68,9 @@
 
     const intit = () => {
         render();
-
         const form = document.querySelector(".js-form");
-
         form.addEventListener("submit", onFormSubmit);
-
     }
 
     intit();
-
 }
